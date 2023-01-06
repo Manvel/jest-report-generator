86e8453a3118b9a750e1ee792ea4ea4618335eca
Manvel Saroyan <saroyanm@gmail.com>
2023-01-06 17:19:00 +0400

    Generate reports

--- report.js
+++ report.js
@@ -1,7 +1,25 @@
+const {writeFile, mkdir} = require("fs/promises");
+const {existsSync} = require("fs");
+const { join } = require("path");
+const targetPath = "reports";
+
 class Reporter {
-  onRunComplete(testContexts, results) {
+  async onRunComplete(testContexts, results) {
     // console.log("results", JSON.stringify(results));
+    const testSuites = results.testResults.map((suite) => {
+      return {tests: suite.testResults.map(({title, status, ancestorTitles, fullName}) => {
+        return {title, status, ancestorTitles, fullName}
+      })};
+    });
+    await saveReport({testSuites});
   }
+}
+
+async function saveReport(data) {
+  if (!existsSync(targetPath)) {
+    await mkdir(targetPath);
+  }
+  await writeFile(join("./", targetPath, "data.json"), JSON.stringify(data, null,2));
 }
 
 module.exports = Reporter;
